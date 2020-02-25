8a5e6c9859f77d75a772933cee689a9b567c271a
jpf <601150880@qq.com>
2020-02-25 21:09:36 +0800

    add store address apis

---
+++ jcart-store-back/src/main/java/io/jpf/jcartstoreback/controller/AddressController.java
@@ -0,0 +1,30 @@
+package io.jpf.jcartstoreback.controller;
+
+import io.jpf.jcartstoreback.dto.in.AddressCreateInDTO;
+import io.jpf.jcartstoreback.dto.in.AddressUpdateInDTO;
+import io.jpf.jcartstoreback.dto.out.AddressListOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+
+    @GetMapping("/getAddressByCustomerId")
+    public List<AddressListOutDTO> getAddressByCustomerId(@RequestAttribute Integer customerId){
+        return null;
+    }
+
+    @PostMapping("/create")
+    public Integer create(@RequestBody AddressCreateInDTO addressCreateInDTO,
+                          @RequestAttribute Integer customerId){
+        return null;
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody AddressUpdateInDTO addressUpdateInDTO,
+                       @RequestAttribute Integer customerId){
+
+    }
+}
