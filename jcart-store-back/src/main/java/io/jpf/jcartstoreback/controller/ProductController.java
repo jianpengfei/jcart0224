388fef3fe2fa4189806934a20f3d15282a71691e
jpf <601150880@qq.com>
2020-03-12 16:30:27 +0800

    modification jcart-store-back product api bug

--- jcart-store-back/src/main/java/io/jpf/jcartstoreback/controller/ProductController.java
+++ jcart-store-back/src/main/java/io/jpf/jcartstoreback/controller/ProductController.java
@@ -1,9 +1,11 @@
 package io.jpf.jcartstoreback.controller;
 
+import com.github.pagehelper.Page;
 import io.jpf.jcartstoreback.dto.in.ProductSearchInDTO;
 import io.jpf.jcartstoreback.dto.out.PageOutDTO;
 import io.jpf.jcartstoreback.dto.out.ProductListOutDTO;
 import io.jpf.jcartstoreback.dto.out.ProductShowOutDTO;
+import io.jpf.jcartstoreback.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +16,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product")
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/search")
     public PageOutDTO<ProductListOutDTO> search(ProductSearchInDTO productSearchInDTO,
                                                 @RequestParam(required = false, defaultValue = "1") Integer pageNum){
 
-        return null;
+        Page<ProductListOutDTO> page = productService.search(pageNum);
+        PageOutDTO<ProductListOutDTO> pageOutDTO = new PageOutDTO<>();
+        pageOutDTO.setTotal(page.getTotal());
+        pageOutDTO.setPageSize(page.getPageSize());
+        pageOutDTO.setPageNum(page.getPageNum());
+        pageOutDTO.setList(page);
+
+        return pageOutDTO;
     }
 
     @GetMapping("/getById")
     public ProductShowOutDTO getById(@RequestParam Integer productId){
-        return null;
+
+        ProductShowOutDTO productShowOutDTO = productService.getShowById(productId);
+        return productShowOutDTO;
     }
 
 }
